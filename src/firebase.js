74421e147cc45fc6de0f5095a66fd85ab8a5c067
destino92 <destinodello@gmail.com>
2017-05-15 01:03:40 +0300

    feat: added firebug to the application

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDr_4lyW507EfeRPbG1ER6XSzAgHPYuQkM",
+  authDomain: "goalcoachdes.firebaseapp.com",
+  databaseURL: "https://goalcoachdes.firebaseio.com",
+  projectId: "goalcoachdes",
+  storageBucket: "goalcoachdes.appspot.com",
+  messagingSenderId: "1084074632425"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
